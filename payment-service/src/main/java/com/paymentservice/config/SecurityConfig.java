1d6baca3d1743da763a9e581dde23b43b99664c8
david.lectamo <juandavidlectamocaicedo@gmail.com>
2025-09-15 16:07:03 -0500

    Added jwt validator to microservices

--- payment-service/src/main/java/com/paymentservice/config/SecurityConfig.java
+++ payment-service/src/main/java/com/paymentservice/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.paymentservice.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,4 +33,12 @@ public class SecurityConfig {
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+            .withSecretKey(new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256"))
+            .build();
+    }
+
 }
