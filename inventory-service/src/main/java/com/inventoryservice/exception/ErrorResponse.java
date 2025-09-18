f1fa9061d775699d8ae218c38c998faa2d6a3bcc
david.lectamo <juandavidlectamocaicedo@gmail.com>
2025-09-18 10:29:17 -0500

    Added custom exeptions

---
+++ inventory-service/src/main/java/com/inventoryservice/exception/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.inventoryservice.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+
+    private String statusDetail;
+
+    private int statusCode;
+
+    private LocalDateTime timeStamp;
+
+    public ErrorResponse(String message, String statusDetail, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+        this.timeStamp = LocalDateTime.now();
+        this.statusDetail = statusDetail;
+    }
+
+}
